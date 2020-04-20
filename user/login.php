209e011470ff65f0f5bb26ed280bc8a173ca6e26
shubham raj <shubhamrajabc@gmail.com>
2020-04-20 17:46:04 +0530

    commit

--- user/login.php
+++ user/login.php
@@ -2,6 +2,8 @@
     <head>
         <title>Healthcare </title>
         <!-- Latest compiled and minified CSS -->
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
         <!-- jQuery library -->
